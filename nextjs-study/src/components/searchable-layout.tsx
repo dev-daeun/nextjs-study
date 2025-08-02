26c17b7345893e285e2ca7e1b0b5da4b72f0a406
dev-daeun <devdaeun@gmail.com>
2025-08-02 14:32:56 +0900

    검색여 입력하고 엔터키 입력하면 /search 이동하도록 이벤트 핸들러 구현

--- nextjs-study/src/components/searchable-layout.tsx
+++ nextjs-study/src/components/searchable-layout.tsx
@@ -10,14 +10,17 @@ export default function SearchableLayout ({children}: {children: ReactNode}) {
     const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
     };
-
     const onSubmitSearchInput = () => {
         if (!search) return;
         router.push(`/search?q=${search}`);
     };
-
+    const onKeyPreesed = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            onSubmitSearchInput();
+        }
+    };
     return (<div>
-        <input placeholder="검색어를 입력하세요" onChange={onChangeSearchInput} value={search}/>
+        <input placeholder="검색어를 입력하세요" onKeyDown={onKeyPreesed} onChange={onChangeSearchInput} value={search}/>
         <button onClick={onSubmitSearchInput}>검색</button>
         {children}
     </div>);

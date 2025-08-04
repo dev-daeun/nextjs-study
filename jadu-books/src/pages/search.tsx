7ab2cd7f127d3040cc461ee946660b1415bb4176
dev-daeun <devdaeun@gmail.com>
2025-08-04 15:35:19 +0900

    검색 결과 페이지에 book item 레이아웃 추가

--- jadu-books/src/pages/search.tsx
+++ jadu-books/src/pages/search.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 import SearchableLayout from "@/layouts/search-layout";
+import BookLayout from "@/layouts/book-layout";
+import books from "@/mock/books.json";
 
 export default function Search () {
     return <>
-    <p>this is search result page</p>
+        {books.map((book) => <BookLayout key={book.id} {...book}/>)}
     </>
 }
 

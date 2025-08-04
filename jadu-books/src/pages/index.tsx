016c347eee0422368d6b3723ef1f3866f7354ab5
dev-daeun <devdaeun@gmail.com>
2025-08-04 16:09:10 +0900

    SSR 개념 학습

--- jadu-books/src/pages/index.tsx
+++ jadu-books/src/pages/index.tsx
@@ -3,8 +3,34 @@ import style from "../styles/index.module.css";
 import SearchableLayout from "@/layouts/search-layout";
 import BookLayout from "@/layouts/book-layout"
 import books from "@/mock/books.json"
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  /* SSR (server-side rendering) 에 필요한 data fetching 담당
+     컴포넌트 함수보다 먼저 실행됨
+     nexjs 서버에서만 실행됨 (브라우저에서 실행 x)
+
+    index 페이지 요청 
+     -> nextjs 서버에서 getServerSideProps() 1회 호출 (= SSR 수행)
+     -> getServerSideProps() 호출 결과를 매개변수로 컴포넌트 함수 호출
+     -> 브라우저는 컴포넌트 함수 호출결과 및 js 번들 수신
+     
+    * 컴포넌트 함수는 nextjs 서버에서 1회(SSR), 브라우저에서 1회(Hydration) 실행됨
+      => 컴포넌트 함수 내에서는 기본적으로 브라우저와 관련된 객체/속성 접근 불가
+         (접근하려면 useEffect() 내부에서 접근 가능)
+  */
+
+  const data = "hello";
+  return {
+    props: {
+      data,
+    }
+  }
+};
+
+
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
   return (
     <div className={style.container}>
       <section>
